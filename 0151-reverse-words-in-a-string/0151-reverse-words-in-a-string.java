09573e5e59651460ac2abfb4ba91dd640a62300e
Abirubha P <abirubhaabi@gmail.com>
2026-09-05 22:27:54 +0530

    Time: 6 ms (80.8%), Space: 44.3 MB (57.37%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -5,7 +5,7 @@ class Solution {
         StringBuilder ans= new StringBuilder();
         for(int i= str.length-1;i>=0;i--){
             ans.append(str[i]);
-            if(i!=0){
+            if(i!=0){ 
                 ans.append(" ");
             }
         }
